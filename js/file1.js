ddcb3f919637283b8b08a879796116d18d568e4c
Rohitpawar0005 <chrohit84@gmail.com>
2025-06-20 10:00:00 +0530

    modified:   js/file1.js 	new file:   js/file2.js 	modified:   js/index.html

--- js/file1.js
+++ js/file1.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+// Introduction to JavaScript
+// console.log("Hello World")
+let age=10;
+let student = "Rohit"
+let promoted = true
+let score
+let city=null
+// console.log(student)
+// console.log(typeof student)
+// console.log(typeof score)
+// console.log(typeof city)
+
+// let strAge = String(age);
+// console.log(typeof strAge)
+// let rating = "5"
+// console.log(typeof rating)
+// let nmRating = Number(rating)
+// console.log(typeof nmRating)
+/*
+let flag = "true"
+let boolFlag = Boolean(flag)
+console.log(typeof boolFlag)
+*/
---
+++ js/file2.js
@@ -0,0 +1,19 @@
+// Operators in JavaScript
+let a = 10
+let b = 54
+
+// console.log(a+b)
+// console.log(b%a)
+// console.log(a%b)
+// let c = "10"
+// console.log(a+c)
+// console.log(a*c)
+// console.log(a-c)
+
+// let d="2"
+// let e=2
+// console.log(d==e)
+// console.log(d===e)
+// console.log(a>b && a>c)
+// console.log(a>b || a>c)
+// console.log(d**e)
