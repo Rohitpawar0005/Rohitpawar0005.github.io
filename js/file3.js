a6c871a9793e2d4c6d745881fa4f1c1dc9e36cc1
Rohitpawar0005 <chrohit84@gmail.com>
2025-06-20 10:23:54 +0530

    new file:   js/file3.js

---
+++ js/file3.js
@@ -0,0 +1,26 @@
+//  in Javascript
+// let a=10;
+// let b=12;
+// if(a>b) console.log(`${a} is greater`)
+// else console.log(`${b} is greater`)
+
+// let c
+// if(c) console.log("Good Morning")
+// else console.log("Good Afternoon")
+
+// let d = null
+// if(d) console.log("Good Morning")
+// else console.log("Good Afternoon")
+
+// let v1 = 10
+// let v2 = 20
+// let v3 = 30
+// if(v1>v2 && v1>v3) console.log('v1 is greater');
+// else if(v2>v1 && v2>v3) console.log('v2 is greater');
+// else console.log('v3 is greater')
+
+let x=5;
+let y=20;
+console.log(x>y? x:y);
+x>y ? console.log('x is greater') : console.log('y is greater');
+x>y && console.log('x is greater');
