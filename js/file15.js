4416ccb9bd651352045004645413346fb482c900
Rohitpawar0005 <chrohit84@gmail.com>
2025-06-24 09:54:55 +0530

    new file:   Portfolio/ShoppingCart/app.js 	new file:   Portfolio/ShoppingCart/index.css 	new file:   Portfolio/ShoppingCart/index.html 	deleted:    js/ShoppingCart/app.js 	deleted:    js/ShoppingCart/index.html 	modified:   js/file15.js 	modified:   js/index.html

--- js/file15.js
+++ js/file15.js
@@ -1,11 +1,21 @@
 //promise
+// const f1 = () => {
+//   setTimeout(() => {
+//     return 5;
+//   }, 1000);
+// };
+// const f2 = (x) => {
+//   console.log(x + 6);
+// };
+// const n = f1();
+// f2(n);
+
 const f1 = () => {
-  setTimeout(() => {
-    return 5;
-  }, 1000);
+  return new Promise((resolve, reject) =>{
+    resolve(5);
+  });
 };
 const f2 = (x) => {
   console.log(x + 6);
 };
-const n = f1();
-f2(n);
+f1().then((n) => f2(n));
