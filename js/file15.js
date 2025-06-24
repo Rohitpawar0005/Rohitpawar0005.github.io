9990b712fa278c268810fe12ed65a6e1f9f32e3b
Rohitpawar0005 <chrohit84@gmail.com>
2025-06-24 11:50:18 +0530

    modified:   js/file15.js 	new file:   js/file16.js 	new file:   js/file17.html 	new file:   js/file18.html

--- js/file15.js
+++ js/file15.js
@@ -10,12 +10,60 @@
 // const n = f1();
 // f2(n);
 
-const f1 = () => {
-  return new Promise((resolve, reject) =>{
-    resolve(5);
-  });
-};
-const f2 = (x) => {
-  console.log(x + 6);
-};
-f1().then((n) => f2(n));
+// const f1 = () => {
+//   return new Promise((resolve, reject) =>{
+//     resolve(5);
+//   });
+// };
+// const f2 = (x) => {
+//   console.log(x + 6);
+// };
+// f1().then((n) => f2(n));
+
+// const f1 = () => {
+//   return new Promise((resolve, reject) =>{
+//     reject("Something went wrong");
+//   });
+// };
+// const f2 = (x) => {
+//   console.log(x + 6);
+// };
+// f1()
+//   .then((n) => f2(n))
+//   .catch((err) => console.log(err))
+
+
+// const f1 = (n) => {
+//   return new Promise((resolve, reject) =>{
+//     if(n<0) reject("Invalid input");
+//     else resolve(n)
+//   });
+// };
+// const f2 = (x) => {
+//   console.log(x + 6);
+// };
+// f1(10)
+//   .then((n) => f2(n))
+//   .catch((err) => console.log(err))
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res)=>res.json())
+//   .then((data)=>{
+//     data.forEach((value) =>{
+//       console.log(`Name-> ${value.name} | Email-> ${value.email}`);
+//     })
+//   })
+//   .catch((err)=>console.log('Something went wrong'));
+
+
+
+// Async, Await Function Method
+  const fetchData = async () =>{
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await res.json()
+    data.forEach((value) =>{
+      console.log(value.name);
+    });
+  };
+  fetchData();
